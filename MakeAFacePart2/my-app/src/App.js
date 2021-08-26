9ed2b90db192c3ccb47de8255eac8df52dfc5195
anwoods <anwoods@ucdavis.edu>
2021-08-26 15:39:35 -0700

    make face part 4 made a react component in same fie appjs

--- MakeAFacePart2/my-app/src/App.js
+++ MakeAFacePart2/my-app/src/App.js
@@ -3,38 +3,54 @@ import * as d3 from 'd3';
 import ReactDOM from 'react-dom';
 import './App.css';
 
-function App() {
-  const width = 960;
-  const height = 500;
+const width = 960;
+const height = 500;
 
-  const centerX = width/ 2;
-  const centerY = height / 2;
-  const strokeWidth = 10
+const centerX = width/ 2;
+const centerY = height / 2;
+const strokeWidth = 10
 
-  const eyeOffsetX = 100;
-  const eyeOffsetY = 100;
-  const eyeRadius = 40;
-  const mouthWidth = 20;
-  const mouthRadius = 150;
+const eyeOffsetX = 100;
+const eyeOffsetY = 100;
+const eyeRadius = 40;
+const mouthWidth = 20;
+const mouthRadius = 150;
 
-  const mouthArc = d3.arc()
-    .innerRadius(mouthRadius)
-    .outerRadius(mouthRadius + mouthWidth)
-    .startAngle(Math.PI / 2)
-    .endAngle(Math.PI * 3/2);
+const mouthArc = d3.arc()
+  .innerRadius(mouthRadius)
+  .outerRadius(mouthRadius + mouthWidth)
+  .startAngle(Math.PI / 2)
+  .endAngle(Math.PI * 3/2);
 
-  return (
-    <div className="App">
+// const BackgroundCircle = (props) => (
+//   <circle 
+//     r={props.radius}
+//     fill="yellow"
+//     stroke="black"
+//     stroke-width={strokeWidth}
+//   >
+//   </circle>
+// );
+
+const BackgroundCircle = ({ radius }) => (
+  <circle 
+    r={radius}
+    fill="yellow"
+    stroke="black"
+    stroke-width={strokeWidth}
+  >
+  </circle>
+);
+
+const App = () => (
+  <div className="App">
       <header className="App-header">
       <svg width={width} height={height}>
         <g transform={`translate(${centerX}, ${centerY})`} >
-          <circle 
-            r={centerY - strokeWidth / 2}
-            fill="yellow"
-            stroke="black"
-            stroke-width={strokeWidth}
+          <BackgroundCircle radius = {centerY - strokeWidth / 2}
           >
-          </circle>
+          </BackgroundCircle>
+
           <circle 
             cx={-eyeOffsetX}
             cy={-eyeOffsetY}
@@ -56,7 +72,62 @@ function App() {
         </svg>
       </header>
     </div>
-  );
-}
+)
+
+// function App() {
+//   const width = 960;
+//   const height = 500;
+
+//   const centerX = width/ 2;
+//   const centerY = height / 2;
+//   const strokeWidth = 10
+
+//   const eyeOffsetX = 100;
+//   const eyeOffsetY = 100;
+//   const eyeRadius = 40;
+//   const mouthWidth = 20;
+//   const mouthRadius = 150;
+
+//   const mouthArc = d3.arc()
+//     .innerRadius(mouthRadius)
+//     .outerRadius(mouthRadius + mouthWidth)
+//     .startAngle(Math.PI / 2)
+//     .endAngle(Math.PI * 3/2);
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//       <svg width={width} height={height}>
+//         <g transform={`translate(${centerX}, ${centerY})`} >
+//           <circle 
+//             r={centerY - strokeWidth / 2}
+//             fill="yellow"
+//             stroke="black"
+//             stroke-width={strokeWidth}
+//           >
+//           </circle>
+//           <circle 
+//             cx={-eyeOffsetX}
+//             cy={-eyeOffsetY}
+//             r={eyeRadius}
+//           >
+//           </circle>
+//           <circle 
+//             cx={eyeOffsetX}
+//             cy={-eyeOffsetY}
+//             r={eyeRadius}
+//           >
+//           </circle>
+//           <path 
+//             d={mouthArc()}
+//           >
+//           </path>
+//         </g>
+        
+//         </svg>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
