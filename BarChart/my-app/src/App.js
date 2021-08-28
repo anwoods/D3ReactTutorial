0d828352b9f0f780b5cce968e1cffe7bab51aee2
anwoods <anwoods@ucdavis.edu>
2021-08-27 17:03:54 -0700

    pathetic bar chart

--- BarChart/my-app/src/App.js
+++ BarChart/my-app/src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+import React, { useState, useCallback, useEffect } from 'react';
+import * as d3 from 'd3';
+
+const csvUrl =
+  'https://gist.githubusercontent.com/curran/0ac4077c7fc6390f5dd33bf5c06cb5ff/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv';
+
+const width = 960;
+const height = 500;
 
 function App() {
+
+
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    // d3.csv(csvUrl).then(data => {
+    //   //console.log("data is ", data);
+    //   setData(data);
+    // });
+    const row = (d) => {
+      d.Population = +d['2020']
+      return d;
+    }
+    d3.csv(csvUrl, row).then(data => {
+      setData(data.slice(0, 10));
+    })
+  }, []);
+
+  if(!data){
+    return <pre>
+      Loading...
+    </pre>
+  }
+
+  const yScale = d3.scaleBand()
+    .domain(data.map(d => d.Country))
+    .range([0, height]);
+
+  const xScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.Population)])
+    .range([0, width]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <svg width={width} height={height}>
+      {
+        data.map(d => 
+          <rect 
+            x={0}
+            y={yScale(d.Country)}
+            width= {xScale(d.Population)}
+            height={yScale.bandwidth()}
+          />
+        )
+      }
+    </svg>
   );
 }
 
